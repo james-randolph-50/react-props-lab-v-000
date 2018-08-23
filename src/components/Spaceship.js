012da53d7cf37e02ec185b37e809ee6ce0ebd76a
James Randolph <james.randolph2016@gmail.com>
2018-08-23 00:21:48 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,5 +1,9 @@
 export default class Spaceship extends React.Component {
   render() {
+    const { name, speed, hasRcokets, color } = this.props;
     
+    return (
+      <div>
+        <Spaceship Name: {name})
   }
 }
